d8f232335c9693ba3e0ef058f3358d063047a6d2
Ramanuj Das <ramanujds9@gmail.com>
2023-12-12 08:48:30 +0530

    Added new example with Spring Rest Repositories

---
+++ student-app-spring-data-rest/src/main/java/com/by/studentapp/StudentAppSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.by.studentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppSpringDataRestApplication.class, args);
+	}
+
+}
---
+++ student-app-spring-data-rest/src/test/java/com/by/studentapp/StudentAppSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by.studentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppSpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

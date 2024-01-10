3835d614514aec82e4e604e53d8911e2e9fe00cd
Ramanuj Das <ramanujds9@gmail.com>
2024-01-10 12:00:02 +0530

    Microservices : Spring Cloud Config Server

--- shopping-app-microservices/shoppingapp-order-ms/src/main/java/com/shoppingapp/orderms/api/OrderServiceController.java
+++ shopping-app-microservices/shoppingapp-order-ms/src/main/java/com/shoppingapp/orderms/api/OrderServiceController.java
@@ -4,23 +4,25 @@ import com.shoppingapp.orderms.model.OrderDetails;
 import com.shoppingapp.orderms.service.OrderDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RefreshScope
 @RequestMapping("/api/orders")
 public class OrderServiceController {
 
     @Autowired
     private OrderDetailsService orderDetailsService;
 
-//    @Value("${message: Default Hello}")
-//    private String message;
-//
-//    @GetMapping
-//    public String sayHello(){
-//        return message;
-//    }
+    @Value("${message: No Message available}")
+    private String message;
+
+    @GetMapping("/message")
+    public String sayHello(){
+        return message;
+    }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
